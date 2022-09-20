0e393728c47fa54c077dfa27818d7d15da95c6d0
Daniyal Ayaz <106926956+universdynamics-daniyalayyaz@users.noreply.github.com>
2022-09-20 17:07:39 +0500

    Login/Signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { BasicDetailsComponent } from './signup/basic-details/basic-details.component';
+import { ContactDetailsComponent } from './signup/contact-details/contact-details.component';
+import { FamilyDetailsComponent } from './signup/family-details/family-details.component';
+import { MorePersonalDetailsComponent } from './signup/more-personal-details/more-personal-details.component';
+import { PersonalDetailsComponent } from './signup/personal-details/personal-details.component';
+import { ResidentialDetailsComponent } from './signup/residential-details/residential-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: LoginComponent},
+  { path:'Basic-Details', component: BasicDetailsComponent},
+  { path:'Personal-Details', component: PersonalDetailsComponent},
+  { path:'More-Personal-Details', component: MorePersonalDetailsComponent},
+  { path:'Residential-Details', component: ResidentialDetailsComponent},
+  { path:'Family-Details', component: FamilyDetailsComponent},
+  { path:'Contact-Details', component: ContactDetailsComponent},
 
+
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { PersonalDetailsComponent } from './signup/personal-details/personal-details.component';
+import { MorePersonalDetailsComponent } from './signup/more-personal-details/more-personal-details.component';
+import { ResidentialDetailsComponent } from './signup/residential-details/residential-details.component';
+import { ProfessionalDetailsComponent } from './signup/professional-details/professional-details.component';
+import { FamilyDetailsComponent } from './signup/family-details/family-details.component';
+import { ContactDetailsComponent } from './signup/contact-details/contact-details.component';
+import { ExtraDetailsComponent } from './signup/extra-details/extra-details.component';
+import { BasicDetailsComponent } from './signup/basic-details/basic-details.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PersonalDetailsComponent,
+    MorePersonalDetailsComponent,
+    ResidentialDetailsComponent,
+    ProfessionalDetailsComponent,
+    FamilyDetailsComponent,
+    ContactDetailsComponent,
+    ExtraDetailsComponent,
+    BasicDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
