24f940240160e01dd8538b5f1d010d2e0a2cc87c
itx-shoaib <shoaibjamil43@gmail.com>
2022-11-17 13:20:41 -0800

    Making values to retrieve on previous button in personal details page

--- src/app/signup/personal-details/personal-details.component.ts
+++ src/app/signup/personal-details/personal-details.component.ts
@@ -7,8 +7,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./personal-details.component.css']
 })
 export class PersonalDetailsComponent implements OnInit {
+  personal:any={name:""};
+
   ReligiousSelect: any;
-  constructor(private router: Router) {}
+  name:any;
+  age:any;
+  ReligiousStatusSelect:any;
+  SectSelect:any;
+  MotherTongueSelect:any;
+  MartialStatusSelect:any;
+  Clan:any;
+  CasteSelect:any;
+  OtherReligion:any;
+
+
+  constructor(private router: Router) {
+    this.name = JSON.parse(localStorage.getItem('PersonalDetails') as string).name
+    this.age = JSON.parse(localStorage.getItem('PersonalDetails') as string).age
+    this.ReligiousSelect = JSON.parse(localStorage.getItem('PersonalDetails') as string).Religious
+    this.SectSelect = JSON.parse(localStorage.getItem('PersonalDetails') as string).Sect
+    this.MotherTongueSelect = JSON.parse(localStorage.getItem('PersonalDetails') as string).MotherTongue
+    this.MartialStatusSelect = JSON.parse(localStorage.getItem('PersonalDetails') as string).MartialStatus
+    this.Clan = JSON.parse(localStorage.getItem('PersonalDetails') as string).Clan
+    this.CasteSelect = JSON.parse(localStorage.getItem('PersonalDetails') as string).Caste
+    this.OtherReligion = JSON.parse(localStorage.getItem('PersonalDetails') as string).OtherReligion
+    this.ReligiousStatusSelect = JSON.parse(localStorage.getItem('PersonalDetails') as string).ReligiousStatus
+  }
   
   ngOnInit(): void {
   }
@@ -130,7 +154,7 @@ MotherTongue=[
     label: 'Other'
   },
 ];
-personal:any={name:""};
+
 castes=["Abbasi", "Abidi", "Afridi", "Agha", "Ahmedani", "Akhoond", "Alvi", "Ansari", "Arain", "Ashkani", "Atcha", "Awan", "Babar", "Badini", "Bahawalanzai", "Bahmani", "Baig", "Bajar", "Bajwa", "Baloch", "Bangash", "Bangial - Rajput", "Bangulzai", "Barazani", "Barija", "Barlas", "Barr", "Basra", "Bhabra", "Bhalli", "Bhamba", "Bhango", "Bhatti", "Bhinder - Jutt", "Bhullar", "Bijarani", "Bizenjo", "Brahmani", "Bugti", "Buledi", "Bulfati", "Burki", "Butt", "Buzdar", "Chachar", "Chagatai", "Chahal", "Chamkanni", "Chandio", "Changwani", "Chatha", "Chaudhry", "Chauhan", "Chechi", "Cheema", "Chishti", "Chughtai", "Chutani", "Damanis", "Dannarzai", "Dar", "Dareshak", "Darzada", "Dasti", "Daulat Khel", "Davi", "dawar", "Derawal", "Dhariwal", "Dhillon", "Dogar", "Domki", "Durrani", "Edo-Khel", "Fareedi", "Farooqi", "Firdausi", "Gabol", "Gadai", "Gadhi", "Gajani", "Gakhar", "Galani", "Gandapur", "Gardezi", "Gashkori", "Ghazali", "Ghazini", "Ghilzai", "Ghuman", "Gichki", "Gill", "Gjar", "Gola", "Gondal", "Gopang", "Goraya", "Gorgage", "Gorshani", "Gujjar", "Gurmani", "Hafiz Khel", "Hamadani", "Hashmi", "Hasni", "Hassani", "Hesbani", "Hooth", "Hussain", "Hussaini", "Hyderi", "Ibrahim", "Idrisi", "Isa Khel", "Isfahani", "Jadgal", "Jadoon - Gadoon", "Jadun", "Jafari", "Jagirani", "Jahangiri", "Jalali", "Jalbani", "Jamali", "Jamshidi", "Jandran", "Janjua - Rajput", "Janjuarajput", "Janwari", "Jappa", "Jarwar", "Jat", "Jatoi", "Jattak", "Jawanda", "Jhalawan", "Jiskani", "Johiya", "Jutt", "Kahloon", "Kakakhel", "Kakar", "Kakazai", "Kallu", "Kalmati", "Kalpar", "Kalyal", "Kambarzahi", "Kamboh", "Kasana", "Kashani", "Kassar", "Kayani", "Kazemi", "Kenagzai", "Kermani", "Khakwani", "Khalil", "Khalol", "Khan", "Khanum", "Khara", "Kharoti", "Khattak", "Khawaja", "Kheazai", "Khetran", "Khokhar", "Khorasani", "Khosa", "Khudiadadzai", "Khulozai", "Khushk", "Kiani", "Kirmani", "Korai", "Kuchelai", "Kuchis", "Kundi", "Kurd", "Lakhani", "Langah", "Langhani", "Lango", "Lanjwani", "Lari", "Lashari", "Leel", "Leghari", "Lehri", "Lodhi", "Loharani", "Lone", "Luna", "Lund", "Maghdud Khel", "Magsi", "Mahar", "Mahmud Khel", "Mahsud Khel", "Mahtam", "Makhdoom", "Malanhaans", "Malik", "Mamund", "Manduri", "Maneka", "Marri", "Marwat", "Mashwanis", "Masood", "Mastoi", "Mazari", "Meghwar", "Mengal", "Meo", "Mian", "Mian Rajput", "Minhas", "Minhas Mengal", "Mir", "Mirwani", "Mirza", "Mohamedzai", "Mohmand", "Montazeri", "Mughal", "Muker", "Mullagori - Mulla Ghori", "Muslim Khatris", "Naeem", "Nanda", "Naqvi", "sherwani", "Niazi", "Nishapuri", "Nizamani", "Noorani", "Noorzai", "Nothazai", "Nutkani", "Orakzai", "Osmani", "Pansota", "Paracha", "Pasha", "Passi", "Pitafi", "Popalzai", "Qaimkhani", "Qaisrani", "Qalat", "Qambrani", "Qazi", "Qizilbash", "Qureshi", "Rahija", "Rahmanzai", "Raisani", "Raja", "Rajput", "Ramay", "Rana", "Ranjha", "Rathore", "Razavi", "Reza", "Rind", "Rizvi", "Rodini", "Sabzvari", "Sadat", "Sadozai", "Sahi", "Sahni", "Sajjadi", "Salarzai", "Salehi", "Sanjarzai", "Sanjrani", "Sarbans", "Sarpara", "Sasooli", "Satti", "Sethwi", "Shahi", "Shahwani", "Shaikh", "Shambhani", "Shanzay", "Sheikh", "Sherzai", "Shilmani", "Shirani", "Sial", "Siddiqui", "Sindhi, Sindh, Pakistan", "Sistani", "Soomrani", "Sulemani", "Suri", "Swati", "Syed", "Tahirkheli", "Talpur", "Tareen", "Tarkani", "Tauqi", "Tirmizi", "Tokhi", "Toubzud", "Turkhel", "Umar Khel", "Umarzai", "Umrani", "Uthman Khel", "Wadeyla", "Wains", "Wasti", "Wazir", "Yazdani", "Yousafzai", "Yusaf Khel", "Zain", "Zand", "Zardari", "Zehri", "Zubairi"] 
 
 eventonKey(event: any) {
