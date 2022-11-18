c0077f639bf9cd6c84db2127db3c4393db6a5049
itx-shoaib <shoaibjamil43@gmail.com>
2022-11-18 01:02:35 -0800

    Making data save on next page button in professional details

--- src/app/signup/professional-details/professional-details.component.ts
+++ src/app/signup/professional-details/professional-details.component.ts
@@ -10,7 +10,24 @@ export class ProfessionalDetailsComponent implements OnInit {
 
   ProfessionSelect: any;
   StatusSelect: any;
-  constructor(private router: Router) {}
+  AnyotherInfo:any;
+  OtherInfo:any;
+  Income:any;
+  Institution:any;
+  Qualification:any;
+  Workplace:any;
+  AlliedJobStatus:any;
+  constructor(private router: Router) {
+    this.ProfessionSelect = JSON.parse(localStorage.getItem('ProfessionalDetails') as string).Profession
+    this.StatusSelect = JSON.parse(localStorage.getItem('ProfessionalDetails') as string).JobStatus
+    this.AlliedJobStatus = JSON.parse(localStorage.getItem('ProfessionalDetails') as string).AlliedJobStatus
+    this.AnyotherInfo = JSON.parse(localStorage.getItem('ProfessionalDetails') as string).AnyotherInfo 
+    this.OtherInfo = JSON.parse(localStorage.getItem('ProfessionalDetails') as string).OtherInfo 
+    this.Income = JSON.parse(localStorage.getItem('ProfessionalDetails') as string).Income
+    this.Institution = JSON.parse(localStorage.getItem('ProfessionalDetails') as string).Institution
+    this.Qualification = JSON.parse(localStorage.getItem('ProfessionalDetails') as string).Qualification
+
+  }
   ngOnInit(): void {
   }
 qualifications=["Allergy", "Andrology", "Anesthesiology", "Cardiacsurgery", "Cardiology", "Community", "Dentistry", "Dermatology", "DiabetesandEndocrinology", "E.N.T", "Gastroenterology", "GeneralPractice", "GeneralSurgery", "Gynae/Obs", "Hepatology", "Medicine", "Nephrology", "Neurology", "Neurosurgery", "Nutrition", "Oncology", "Ophthalmology", "Orthopedics", "Paediatricsurgery", "Paediatrics", "Pathology", "Physiotherapy", "PlasticSurgery", "Psychiatry", "PsychologyPulmonology", "Radiology", "Rheumatology", "Urology"]
