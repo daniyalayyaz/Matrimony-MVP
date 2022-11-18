0658e771dfb74a30677dfb7126be7431a98c8c26
itx-shoaib <shoaibjamil43@gmail.com>
2022-11-18 00:07:50 -0800

    Making values to retrieve on previous button in residential details page

--- src/app/signup/residential-details/residential-details.component.ts
+++ src/app/signup/residential-details/residential-details.component.ts
@@ -9,7 +9,20 @@ import { Router } from '@angular/router';
 export class ResidentialDetailsComponent implements OnInit {
 
  CountrySelect: any;
-  constructor(private router: Router) {}
+ Province:any;
+ HouseStatus:any;
+ FuturePlanstoLiveIn:any;
+ City:any;
+ AnyOtherNationality:any;
+ 
+  constructor(private router: Router) {
+    this.City = JSON.parse(localStorage.getItem('ResidentialDetails') as string).City
+    this.AnyOtherNationality = JSON.parse(localStorage.getItem('ResidentialDetails') as string).AnyOtherNationality
+    this.FuturePlanstoLiveIn = JSON.parse(localStorage.getItem('ResidentialDetails') as string).FuturePlanstoLiveIn
+    this.HouseStatus = JSON.parse(localStorage.getItem('ResidentialDetails') as string).HouseStatus
+    this.Province = JSON.parse(localStorage.getItem('ResidentialDetails') as string).Province
+    this.CountrySelect = JSON.parse(localStorage.getItem('ResidentialDetails') as string).Country
+  }
   ngOnInit(): void {
   }
 countries=["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czechia (Czech Republic)", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Holy See", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar (formerly Burma)", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine State", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"]
@@ -25,7 +38,7 @@ gotoSignupThirdPage(){
 gotoSignupSixthPage(){
   localStorage.setItem('ResidentialDetails', JSON.stringify(this.ResidentialDetails))
     console.log(JSON.parse(localStorage.getItem('ResidentialDetails') as string))
-  this.router.navigate(['Professional-Details']);
+  // this.router.navigate(['Professional-Details']);
 }
 
 
