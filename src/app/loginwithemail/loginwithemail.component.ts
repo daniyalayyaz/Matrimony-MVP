51d6524cc6a9c3e1611ebe4bd117cd3e5ac7e8a6
itx-shoaib <shoaibjamil43@gmail.com>
2022-12-29 17:37:46 +0500

    Integrating Profilelogin api

--- src/app/loginwithemail/loginwithemail.component.ts
+++ src/app/loginwithemail/loginwithemail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-loginwithemail',
@@ -7,12 +8,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./loginwithemail.component.css']
 })
 export class LoginwithemailComponent implements OnInit {
-
-  constructor(private router: Router) { }
-
+  email: string;
+  password: string;
+  constructor(private router: Router,private http: HttpClient) { }
+  LoginDetails:any={};
   ngOnInit(): void {
   }
   gotoSignupFirstPage(){
     this.router.navigate(['Basic-Details']);}
 
+    gotoVerification(){
+      if (!this.email || !this.password) {
+        // Show error message if email or password is empty
+        console.log('Email and password are required');
+        return;
+      }
+  
+      const body = {
+        email: this.email,
+        password: this.password
+      };
+      // Integrating Profilelogin API
+      this.http.post('http://localhost:5000/api/user/Profilelogin', body).subscribe(response => {
+        if (response.hasOwnProperty('id')) {
+          console.log(response);
+          // Saving response of API (JWT Token) in localStorage
+          localStorage.setItem('CurrentUser', JSON.stringify(response))
+          this.router.navigate(['dashboard']);
+        } else {
+          console.log(response);
+        }
+
+      });
+    }
 }
