9ccf1f9d2f3c54ec8ce061b3eb8f9c21346c90d4
rizwan136 <rizwantaj57776@gmail.com>
2023-01-30 22:44:13 +0500

    fix

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -12,125 +12,107 @@ import { User } from '../models/user.modal';
   templateUrl: './favourites.component.html',
   styleUrls: ['./favourites.component.css']
 })
-export class FavouritesComponent  extends UnsubscribeHandelr  implements OnInit {
+export class FavouritesComponent extends UnsubscribeHandelr implements OnInit {
 
   constructor(private router: Router,
     private appService: AppService, private toasterservice: ToastrService,) {
 
-      super()
-     }
+    super()
+  }
 
   ngOnInit(): void {
-   this.getfavlist();
+    this.getfavlist();
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
-  personList: Array<User> = [];
+  personList: any = [];
   pathmessage: string = "../../assets/message.png";
   pathheart: string = "../../assets/pinkheart.png";
 
- 
-personList2=[{
-  name: "Raffy",
-  age : 20,
-  gender: "male",
-  color:"white",
-  city:"lahore",
-  caste:"y",
-  height:5.2,
-  professional:"developer",
-  appearance:"good",
 
+  personList2 = [{
+    name: "Raffy",
+    age: 20,
+    gender: "male",
+    color: "white",
+    city: "lahore",
+    caste: "y",
+    height: 5.2,
+    professional: "developer",
+    appearance: "good",
+  },
+  {
+    name: "Murtaza",
+    age: 23,
+    gender: "male",
+    color: "white",
+    city: "islamabad",
+    caste: "y",
+    height: 5.2,
+    professional: "technician",
+    appearance: "not bad",
+  },
+  {
+    name: "Ali",
+    age: 24,
+    gender: "male",
+    color: "white",
+    city: "sailkot",
+    caste: "y",
+    height: 5.3,
+    professional: "front",
+    appearance: "bsml",
+  },
+  {
+    name: "Sherjeel",
+    age: 19,
+    gender: "male",
+    color: "white",
+    city: "lahore",
+    caste: "y",
+    height: 5.2,
+    professional: "devel",
+    appearance: "enough",
 
+  }
+  ]
 
-},
-{
-  name: "Murtaza",
-  age : 23,
-  gender: "male",
-  color:"white",
-  city:"islamabad",
-  caste:"y",
-  height:5.2,
-  professional:"technician",
-  appearance:"not bad",
+  gotoInterests() {
+    this.router.navigate(['Interests']);
+  }
 
+  AddtoFavClick(person: User) {
+    this.appService.AddRemoveFavourite(this.CurrentUser._id, person._id)
+      .pipe(takeUntil(this.Unsubscribe$)).subscribe(response => {
+        this.toasterservice.success("Person has been added to Favourites Successfully!");
+        this.getfavlist()
+      })
+  }
 
+  gotoProfile(person: User) {
+    localStorage.setItem(LocalStorageItem.SELECTED_PROFILE, JSON.stringify(person));
+    this.router.navigate(['Profile']);
+  }
 
-},
-{
-  name: "Ali",
-  age : 24,
-  gender: "male",
-  color:"white",
-  city:"sailkot",
-  caste:"y",
-  height:5.3,
-  professional:"front",
-  appearance:"bsml",
-
-
-
-},
-{
-  name: "Sherjeel",
-  age : 19,
-  gender: "male",
-  color:"white",
-  city:"lahore",
-  caste:"y",
-  height:5.2,
-  professional:"devel",
-  appearance:"enough",
-
-
-
-}
-
-
-]
-gotoInterests() {
-  this.router.navigate(['Interests']);
-}
-
-AddtoFavClick(person: User) {
-  this.appService.AddRemoveFavourite(this.CurrentUser._id, person._id)
-  .pipe(takeUntil(this.Unsubscribe$)).subscribe(response => {
-    this.toasterservice.success("Person has been added to Favourites Successfully!");
-    this.getfavlist()
-  })
-}
- 
-gotoProfile(person: User) {
-  localStorage.setItem(LocalStorageItem.SELECTED_PROFILE,JSON.stringify(person));
-  this.router.navigate(['Profile']);
-}
-getfavlist(){
-  let loggedUser = localStorage.getItem(LocalStorageItem.LOGGED_USER);
-  if(loggedUser) {
-  this.CurrentUser = JSON.parse(loggedUser);
-  this.appService.getfav(this.CurrentUser._id).pipe(
-    takeUntil(this.Unsubscribe$))
-    .subscribe((persons: Array<User>) => 
-      {
-       
-        this.personList = [];
-        this.personList = persons;
-        console.warn(this.personList)
-        // console.warn(this.personList)
-        // console.warn(this.personList)
-        if(this.personList.length>0)
-        {
-         this.toasterservice.success(`Record Found Successfully!`);
+  getfavlist() {
+    let loggedUser = localStorage.getItem(LocalStorageItem.LOGGED_USER);
+    if (loggedUser) {
+      this.CurrentUser = JSON.parse(loggedUser);
+      this.appService.getfav(this.CurrentUser._id).pipe(
+        takeUntil(this.Unsubscribe$))
+        .subscribe((persons: Array<User>) => {
+          this.personList = persons;
+          console.warn(this.personList)
+          // console.warn(this.personList)
+          // console.warn(this.personList)
+          if (this.personList.length > 0) {
+            this.toasterservice.success(`Record Found Successfully!`);
+          }
+          else {
+            this.toasterservice.error("NO RECORD FOUND!");
+          }
         }
-        else{
-          this.toasterservice.error("NO RECORD FOUND!");
-
-        }
-      }  
-    
-    );
+        );
     }
-  
-}
+  }
 }
